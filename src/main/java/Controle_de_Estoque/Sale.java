34dc73db7fbe6f64e64652ea157c1c26837ad46e
Caique Pires <c.pires@ba.estudante.senai.br>
2025-02-10 04:20:45 -0300

    Estrutura da venda: Adicionada a classe Sale para representar uma venda de produto, com cálculo do valor total e atualização do estoque do produto.

---
+++ src/main/java/Controle_de_Estoque/Sale.java
@@ -0,0 +1,27 @@
+package Controle_de_Estoque;
+
+public class Sale {
+    private int id;
+    private Product product;
+    private int quantity;
+    private double valueSale;
+
+    // Metodo Construtor para criar a venda do produto
+    Sale(int id, Product product, int quantity){
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+
+        // Realizando calculo do total da venda do produto
+        valueSale = product.getPrice() * quantity;
+
+        // Decrementando quantidade do estoque
+        product.setQuantity(quantity);
+    }
+
+    // Metodo para exibir informações da venda e do produto
+    public void SellInfo(){
+        System.out.println("\n\t--------INFORMAÇÕES DA VENDA--------");
+        System.out.println("Id: " + id + " Qtd: " + quantity + " Valor total R$" + valueSale);
+    }
+}
