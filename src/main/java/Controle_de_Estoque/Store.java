112e08b029cde17a1bf9f8b3a9de2c5c469543da
Caique Pires <c.pires@ba.estudante.senai.br>
2025-02-10 04:21:52 -0300

    Estrutura da da loja criada: Essa classe serve como um "container" para manter as informações dos produtos e das vendas, permitindo o gerenciamento desses dados de maneira organizada.

---
+++ src/main/java/Controle_de_Estoque/Store.java
@@ -0,0 +1,10 @@
+package Controle_de_Estoque;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Classe para armaazenar produtos e vendas cadastrados na loja
+public class Store {
+    List<Product>products = new ArrayList<>();
+    List<Sale>sales = new ArrayList<>();
+}
