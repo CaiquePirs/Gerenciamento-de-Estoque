75e2caf42bd6c8a507ec3c10108c3ac2c7e59966
Caique Pires <c.pires@ba.estudante.senai.br>
2025-02-13 23:00:32 -0300

    Atualização Store.java

--- src/main/java/Controle_de_Estoque/Store.java
+++ src/main/java/Controle_de_Estoque/Store.java
@@ -3,7 +3,7 @@ package Controle_de_Estoque;
 import java.util.ArrayList;
 import java.util.List;
 
-// Classe para armaazenar produtos e vendas cadastrados na loja
+// Classe para armaazenar produtos e vendas cadastradas na loja
 public class Store {
     List<Product>products = new ArrayList<>();
     List<Sale>sales = new ArrayList<>();
