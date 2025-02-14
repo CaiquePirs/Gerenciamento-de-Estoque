382451b1d52d26db436ef788a6c8d285e7c91f32
Caique Pires <c.pires@ba.estudante.senai.br>
2025-02-13 22:58:48 -0300

    Atualizações Product.java

--- src/main/java/Controle_de_Estoque/Product.java
+++ src/main/java/Controle_de_Estoque/Product.java
@@ -1,13 +1,13 @@
 package Controle_de_Estoque;
 
 public class Product {
-   // Variaáveis para armazenar informações do produto
+   // Variáveis para armazenar informações do produto
     private int id;
     private int quantity;
     private double price;
     private String name;
 
-    // Metodo construtor para criar o produto
+    // Método construtor para criar o produto
     Product(int id, int quantity, double price, String name){
         this.id = id;
         this.quantity = quantity;
@@ -15,21 +15,22 @@ public class Product {
         this.name = name;
     }
 
-    // Getters
+    // Getter para retornar o preço do produto
     public double getPrice() {
         return price;
     }
 
+     // Getter para retornar a quantidade do produto
     public int getQuantity(){
         return quantity;
     }
 
-    // Metodo Set para decrementar quantidade do produto quando vendido
+    // Método Set para decrementar a quantidade no estoque do produto quando vendido
     public int setQuantity(int quantity){
         return this.quantity -= quantity;
     }
 
-    // Metodo para exibir informações do produto cadastrado
+    // Método para exibir informações do produto cadastrado
     public String toString() {
         return "Produto [ID: " + id + ", Nome: " + name + ", Estoque: " + quantity + ", Preço: R$" + price + "]";
     }
