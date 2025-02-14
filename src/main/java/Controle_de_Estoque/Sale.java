0bb67bcc7193b4eba55ea7ccacb4019b1debc5b6
Caique Pires <c.pires@ba.estudante.senai.br>
2025-02-13 23:00:04 -0300

    Atualizações Sale.java

--- src/main/java/Controle_de_Estoque/Sale.java
+++ src/main/java/Controle_de_Estoque/Sale.java
@@ -6,20 +6,20 @@ public class Sale {
     private int quantity;
     private double valueSale;
 
-    // Metodo Construtor para criar a venda do produto
+    // Método Construtor para criar a venda do produto
     Sale(int id, Product product, int quantity){
         this.id = id;
         this.product = product;
         this.quantity = quantity;
 
-        // Realizando calculo do total da venda do produto
+        // Realizando o calculo total da venda do produto
         valueSale = product.getPrice() * quantity;
 
         // Decrementando quantidade do estoque
         product.setQuantity(quantity);
     }
 
-    // Metodo para exibir informações da venda e do produto
+    // Método para exibir as informações da venda
     public void SellInfo(){
         System.out.println("\n\t--------INFORMAÇÕES DA VENDA--------");
         System.out.println("Id: " + id + " Qtd: " + quantity + " Valor total R$" + valueSale);
